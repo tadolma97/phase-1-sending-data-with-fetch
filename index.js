f7750aadbc17e24a3391fb58cd91c09ba7bf2b36
Tashi Dolma <tadolma97@gmail.com>
2022-05-06 15:43:03 -0400

    commit tree 4f8070ddbef6e4734ef1f3052ad1d2ca9a74d110

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {        
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        })
+    })
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data){
+        document.body.textContent=data.id
+    })
+    .catch(function (error){
+        document.body.textContent=error.message
+    })
+}
+submitData()
